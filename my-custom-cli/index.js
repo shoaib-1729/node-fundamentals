c722919abcc0ae5047a076690ad5628a078d9bd1
shoaib-1729 <shoaibahktar56@gmail.com>
2025-01-16 22:05:34 +0530

    building cli tools

--- my-custom-cli/index.js
+++ my-custom-cli/index.js
@@ -1,3 +1,43 @@
-#!/user/bin/env node
+#!/usr/bin/env node
 
-console.log("code-evolution pokedex")
+// for cli options (using 'yargs' npm package)
+// import yargs
+// const yargs = require('yargs');
+// yeh cli ke saare arguments ko key value pair mei convert kar dega (interpreter, cli path, options)
+// const { argv } = yargs(process.argv)
+// console.log(argv);
+// cli options
+// console.log(process.argv);
+
+// for cli interactivity (using inquirer npm package)
+// import inquirer
+const inquirer = require('inquirer')
+    // create prompt module
+const prompt = inquirer.createPromptModule();
+// prompt questions
+prompt([{
+        "type": "input",
+        "name": "pokemon",
+        "message": "Enter a pokemon name to view its 5 moves"
+    }])
+    // prompt method returns a promise with user responses over CLI
+    .then((answers) => {
+        const pokemon = answers.pokemon;
+        // pass-in the pokemon entered by the user over CLI tool
+        printFiveMoves(pokemon);
+    });
+
+
+const printFiveMoves = async(pokemonName) => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+    const data = await response.json();
+    // destructure move property
+    const moves = data.moves;
+    // map over moves to fetch each move name
+    const move = moves.map(({ move }) => move.name)
+        // get only first five moves
+    console.log(move.slice(0, 5))
+}
+
+// use this in case of cli options
+// printFiveMoves(argv.pokemon)
---
+++ my-custom-cli/readme.md
@@ -0,0 +1,7 @@
+# Building your own CLI Tool:
+
+1. Building your own CLI tool
+2. Passing CLI Options
+3. Adding Interactivity to CLI
+
+
